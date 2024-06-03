8c4f9144f84d7fc2f909dd4fba41cea9c5bb106f
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-06-03 15:19:43 +0200

    attemp at playing mp3/wav file

--- spikes/src/SoundEffects.tsx
+++ spikes/src/SoundEffects.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import { drumloop } from './assets/drumloop.mp3'
 
 const SoundEffects: React.FC = () => {
+
     const playSfx = () => {
-        console.log("hello")
+        const sample = new Audio('assets/flynn.wav')
+        sample.play()
+        // const audioTag = document.getElementById("audioTag") as HTMLAudioElement
+        // if(audioTag === null) {
+        //     console.log("Could not find audioTag")
+        //     return
+        // }
+        // audioTag.play()
+        // console.log("hello")
     }
+
     return (
         <div>
-            <audio src={"drumloop.mp3"} id="audioTag"></audio>
             <button onClick={playSfx}>PLAY SFX</button>
         </div>
     );
