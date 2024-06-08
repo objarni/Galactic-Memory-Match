2d65f88bb98153b66d69e9da89357e312457a194
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-06-08 14:48:09 +0200

    sprite (wip) component with image absolutely positioned

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import ScaleText from 'react-scale-text';
+import Image from './Ring.png';
 import './App.css';
 import Card from './Card';
 
 const params = '?' + window.location.href.split('?')[1]
 const queryParams = new URLSearchParams(params)
-const sf: number= queryParams.get("sf") === null ? 1 : queryParams.get("sf") as unknown as number
+const sf: number = queryParams.get("sf") === null ? 1 : queryParams.get("sf") as unknown as number
 
-console.log("Found scale-factor (sci-fi): ", sf)
+console.log("Scale factor (sci-fi): ", sf)
+
+function Sprite(props: { image: string, pos: { x: number; y: number }, scale: number }) {
+    const style: React.CSSProperties = {
+        position: 'absolute',
+        left: props.pos.x * props.scale * 10,
+        top: props.pos.y * props.scale,
+        transform: `scale(${props.scale})`
+    };
+    return <img src={props.image} alt="" style={style}/>
+}
 
 function App() {
+    const p = {x: 2, y: 100}
     return (
         <div className="App">
-            <Card text={"A"} scale={sf} />
+            <Card text={"A"} scale={sf}/>
+            <Sprite image={Image} pos={p} scale={sf}/>
         </div>
     );
 }
